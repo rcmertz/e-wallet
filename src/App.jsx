f45673bae9990da3bdfe45dc64ca02a7ac95d0c7
rcmertz <rcliberalli@hotmail.com>
2022-11-14 17:36:25 -0300

    Expense

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Login from './components/login/Login';
+import NewExpense from './components/expense/NewExpense';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/' element={<NewExpense />} />
       </Routes>
     </BrowserRouter>
   );
