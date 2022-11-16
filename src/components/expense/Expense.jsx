ee8035be30fc79e5a63f1015a3f20eab9c213eb5
Vinícius Frasson <vinifrasson13@gmail.com>
2022-11-16 20:55:10 -0300

    expense-form

--- src/components/expense/Expense.jsx
+++ src/components/expense/Expense.jsx
@@ -5,21 +5,34 @@ import "./Expense.css"
 
 export default function Expense() {
   return (
-    <div className="expense">
+    <div className="expense-main">
       <div className="expense-action">
-        <div className="form">
-          <div className="form2">
-            <p>teste
-              <input type="text" />
-            </p>
-            <p>teste2
-              <input type="text" />
-            </p>
-            <p>
-              teste
-              <input type="text" />
-            </p>
-          </div>
+        <div className="titulo">
+          <h1>NOVO DESPESA</h1>
+          <h1>+</h1>
+        </div>
+        <div className="expense-form1">
+        <select name="moeda">
+          <option value="valor1">Real</option>
+          <option value="valor2">Dolar</option>
+          <option value="valor3">Euro</option>
+        </select>
+
+        <Input id='valor' type='number'>Valor</Input>
+        </div>
+
+        <div className="expense-form2">
+        <Input id='descricao' type='text'>Descriçao</Input>
+        </div>
+
+        <div className="expense-form3">
+        <select name="Forma de Pagamento">
+          <option value="valor1">Dinheiro</option>
+          <option value="valor2">PIX</option>
+          <option value="valor3">Cartao</option>
+        </select>
+
+        <Input id='categoria' type='text'>Categoria</Input>
 
         </div>
       </div>
