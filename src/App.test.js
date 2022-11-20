9dc21058dc111e30cc928c4f9e1bd2c5771cad41
rcmertz <rcliberalli@hotmail.com>
2022-11-20 20:12:19 -0300

    login-validate

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,120 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Login", () => {
+  test("given email, when empty, then show required error message", async () => {
+    render(<App />);
+
+    const email = screen.getByTestId("email");
+
+    await userEvent.type(email, "anyValue");
+    await userEvent.clear(email);
+
+    const requiredError = screen.queryByTestId("email-required");
+    expect(requiredError).not.toBeNull();
+  });
+
+  test("given email, when has value, then hide required error message", async () => {
+    render(<App />);
+
+    const email = screen.getByTestId("email");
+
+    await userEvent.type(email, "anyValue");
+
+    const requiredError = screen.queryByTestId("email-required");
+    expect(requiredError).toBeNull();
+  });
+
+  test("given email, when field not changed, then hide required error message", async () => {
+    render(<App />);
+
+    const requiredError = screen.queryByTestId("email-required");
+    expect(requiredError).toBeNull();
+  });
+
+  test("given email, when invalid, then show invalid error message", async () => {
+    render(<App />);
+
+    const email = screen.getByTestId("email");
+
+    await userEvent.type(email, "anyValue");
+
+    const requiredError = screen.queryByTestId("email-invalid");
+    expect(requiredError).not.toBeNull();
+  });
+
+  test("given email, when valid, then hide invalid error message", async () => {
+    render(<App />);
+
+    const email = screen.getByTestId("email");
+
+    await userEvent.type(email, "valid@email.com");
+
+    const requiredError = screen.queryByTestId("email-invalid");
+    expect(requiredError).toBeNull();
+  });
+
+  test("given password, when empty, then show required error message", async () => {
+    render(<App />);
+
+    const password = screen.getByTestId("password");
+
+    await userEvent.type(password, "anyValue");
+    await userEvent.clear(password);
+
+    const requiredError = screen.queryByTestId("password-required");
+    expect(requiredError).not.toBeNull();
+  });
+
+  test("given password, when has value, then hide required error message", async () => {
+    render(<App />);
+
+    const password = screen.getByTestId("password");
+
+    await userEvent.type(password, "anyValue");
+
+    const requiredError = screen.queryByTestId("password-required");
+    expect(requiredError).toBeNull();
+  });
+
+  test("given email, when empty, then disable recover password button", () => {
+    render(<App />);
+
+    const recoverPasswordButton = screen.getByTestId("recover-password-button");
+
+    expect(recoverPasswordButton).toBeDisabled();
+  });
+
+  test("given email, when valid, then enable recover password button", () => {
+    render(<App />);
+
+    const email = screen.getByTestId("email");
+    userEvent.type(email, "valid@email.com");
+
+    const recoverPasswordButton = screen.getByTestId("recover-password-button");
+
+    expect(recoverPasswordButton).not.toBeDisabled();
+  });
+
+  test("given form invalid, then disable login button", () => {
+    render(<App />);
+
+    const loginButton = screen.getByTestId("login-button");
+
+    expect(loginButton).toBeDisabled();
+  });
+
+  test("given form valid, then enable login button", () => {
+    render(<App />);
+
+    const email = screen.getByTestId("email");
+    userEvent.type(email, "gustavo@gmail.com");
+    const password = screen.getByTestId("password");
+    userEvent.type(password, "gustavo1234");
+
+    const loginButton = screen.getByTestId("login-button");
+
+    expect(loginButton).not.toBeDisabled();
+  });
 });
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,9 +2,26 @@ import Input from "../input/Input";
 import Button from "../button/Button";
 import "./Login.css"
 import { Link } from 'react-router-dom'
+import React, { useState } from "react";
 export default function Login() {
-  return (
 
+
+  const [form, setForm] = useState({
+    email: {
+      hasChanged: false,
+      value: "",
+    },
+    password: {
+      hasChanged: false,
+      value: "",
+    },
+  });
+
+  const isEmailValid = (email) => {
+    return /\S+@\S+.\S+/.test(email);
+  };
+
+  return (
     <div className="login-container">
       <div className="titulo">
         <h1>CARTEIRA DIGITAL</h1>
@@ -18,17 +35,68 @@ export default function Login() {
 
           <div className="input-group">
             <div className="text-input">
-              <Input id='email' type='email'>Email</Input>
+              <h3>Email</h3>
+              <input
+                id="email"
+                className="email"
+                type="email"
+                placeholder="Email"
+                value={form.email.value}
+                onChange={(event) =>
+                  setForm({
+                    ...form,
+                    email: {
+                      hasChanged: true,
+                      value: event.target.value,
+                    },
+                  })
+                }
+                data-testid="email"
+              />
+              {form.email.hasChanged && !form.email.value && (
+                <div data-testid="email-required">Email é obrigatório</div>
+              )}
+              {form.email.hasChanged && !isEmailValid(form.email.value) && (
+                <div data-testid="email-invalid">Email é inválido</div>
+              )}
             </div>
             <div className="text-input">
-              <Input id='senha' type='password'>Senha</Input>
+              {/* <Input id='senha' type='password'>Senha</Input> */}
+              <h3>Senha</h3>
+              <input
+                className="input-style"
+                type="password"
+                placeholder="Senha"
+                value={form.password.value}
+                onChange={(event) =>
+                  setForm({
+                    ...form,
+                    password: {
+                      hasChanged: true,
+                      value: event.target.value,
+                    },
+                  })
+                }
+                data-testid="password"
+              />
+              {form.password.hasChanged && !form.password.value && (
+                <div data-testid="password-required">Senha é obrigatória</div>
+              )}
             </div>
           </div>
 
           <div className="button" style={{ marginLeft: "35%" }}>
             <Link to="/wallet">
-              <Button id='login-button'>Entrar</Button>
+              <button
+                id="login-button"
+                type="button"
+                data-testid="login-button"
+                disabled={!isEmailValid(form.email.value) || !form.password.value}
+              >
+                ENTRAR
+              </button>
             </Link>
+            {/* <Button id='login-button'>Entrar</Button> */}
           </div>
 
         </form>
